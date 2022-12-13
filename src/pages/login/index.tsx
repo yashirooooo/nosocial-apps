fabbf9b0b19d116531d7771cd95cc9cd2170413a
LowEntropyBody <jszyyx@163.com>
2022-12-13 17:07:28 +0800

    remove usless code

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,6 @@
 import Button from '@mui/material/Button';
-import useMetaMask, { MetaMaskProvider } from '../../components/wallets/metamask';
-import metamaskIcon from '../../assets/svgs/metamask.svg';
-import nounWavingIcon from '../../assets/svgs/noun_waving.svg';
+import useMetaMask from '../../components/wallets/metamask';
+import metamaskIcon from '../../assets/images/metamask.png';
 import './style.css';
 
 function LoginPage() {
@@ -9,14 +8,8 @@ function LoginPage() {
     return (
         <div className="Login">
             <header className="Login-header">
-                <Button variant="contained" onClick={connect} disabled={isConnected}>
+                <Button variant="outlined" onClick={connect} disabled={isConnected}>
                     <img src={metamaskIcon} alt="MetaMask" width="50" height="50" /> Connect to MetaMask
-                </Button>
-                <div className="mt-2 mb-2">
-                    Connected Account: {isConnected ? account : ''}
-                </div>
-                <Button variant="contained" onClick={disconnect}>
-                    <img src={nounWavingIcon} width="50" height="50" /> Disconnect MetaMask
                 </Button>
             </header>
         </div>
