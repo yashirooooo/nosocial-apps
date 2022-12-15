fc086b03bde875be2f13e190f9527b66d93384eb
yuhui <1161569428@qq.com>
2022-12-15 15:52:54 +0800

    Activities and benefits

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Container, Typography } from '@mui/material';
+import { Box, Card, CardContent, CardMedia, Container, TextField, Typography } from '@mui/material';
 import './style.css';
 // components
 
@@ -15,7 +15,9 @@ export default function ProfileAppPage() {
       </Helmet>
       {/* <Container maxWidth="xl">
       </Container> */}
-      <div className='background' />
+      <div className='cover_img' >
+        <img src='/assets/images/1028306.png' />
+      </div>
       <div className='container'>
         <div className='basic'>
           <div className='basic_info'>
@@ -32,6 +34,168 @@ export default function ProfileAppPage() {
                 <div id='followings'>10 Followings |</div>
                 <div id='followers'>9,765 Followers |</div>
                 <div id='birthday'>Joined March 2020</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='profile_list'>
+          <div className='category_container'>
+            <div>
+              <TextField
+                id="standard-read-only-input"
+                defaultValue="AI Tags"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </div>
+            <div className='category_data_list'>
+
+            </div>
+          </div>
+          <div className='category_container'>
+            <div>
+              <TextField
+                id="standard-read-only-input"
+                defaultValue="Achievements"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </div>
+            <div className='category_data_list'>
+
+            </div>
+          </div>
+          <div className='category_container'>
+            <div>
+              <TextField
+                id="standard-read-only-input"
+                defaultValue="Activities"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </div>
+            <div className='category_data_list'>
+              <div className='category_data_list_cl'>
+                
+                <div className='activity_data_card'>
+                  <Card>
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Posts
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        76
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: '#9AE4A7' }}>
+                        +11% of last week
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className='activity_data_card'>
+                  <Card>
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Mirrors
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        19,231
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: '#EF8F8F' }}>
+                        -56.6% of last week
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className='activity_data_card'>
+                  <Card>
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Comments
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        156
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: '#9AE4A7' }}>
+                        +17% of last week
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+                <div className='activity_data_card'>
+                  <Card>
+                    <CardContent>
+                      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Collects
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        156
+                      </Typography>
+                      <Typography variant="body2" sx={{ color: '#9AE4A7' }}>
+                        +17% of last week
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+          <div className='category_container'>
+            <div>
+              <TextField
+                id="standard-read-only-input"
+                defaultValue="Benefits"
+                InputProps={{
+                  readOnly: true,
+                }}
+                variant="standard"
+              />
+            </div>
+            <div className='category_data_list'>
+              <div className='category_data_list_cl'>
+                <div className='benefits_data_card'>
+                  <img id='icon' src='/assets/images/image2.svg' />
+                  <div id='desc'>
+                    <div id='top'>1.23 Ethers</div>
+                    <div id='bottom'>by lenster</div>
+                  </div>
+                  <img id='img' src='/assets/images/image1.svg' />
+                </div>
+                <div className='benefits_data_card'>
+                  <img id='icon' src='/assets/images/image2.svg' />
+                  <div id='desc'>
+                    <div id='top'>1.23 Ethers</div>
+                    <div id='bottom'>by lenster</div>
+                  </div>
+                  <img id='img' src='/assets/images/image1.svg' />
+                </div>
+
+              </div>
+              <div className='category_data_list_cl'>
+                <div className='benefits_data_card'>
+                  <img id='icon' src='/assets/images/image2.svg' />
+                  <div id='desc'>
+                    <div id='top'>1.23 Ethers</div>
+                    <div id='bottom'>by lenster</div>
+                  </div>
+                  <img id='img' src='/assets/images/image1.svg' />
+                </div>
+
+                <div className='benefits_data_card'>
+                  <img id='icon' src='/assets/images/image2.svg' />
+                  <div id='desc'>
+                    <div id='top'>1.23 Ethers</div>
+                    <div id='bottom'>by lenster</div>
+                  </div>
+                  <img id='img' src='/assets/images/image1.svg' />
+                </div>
               </div>
             </div>
           </div>
