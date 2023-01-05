2b631b62e788b5c8607cd288b35c6910daa0071a
LowEntropyBody <jszyyx@163.com>
2023-01-05 15:36:30 +0800

    fix err

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -17,8 +17,6 @@ export default function ProfileAppPage() {
       <Helmet>
         <title> Profile | NoSocial Apps </title>
       </Helmet>
-      {/* <Container maxWidth="xl">
-      </Container> */}
       <div>
         <div className='cover_img' >
           <img src='/assets/images/1028306.png' />
@@ -68,11 +66,16 @@ export default function ProfileAppPage() {
                 <AchievementsDetailCard name={'Videos'} amount={6 / 10} />
               </div>
             </div>
-            <div className='basic_info_data'>
-              <div id='name'>Bob Dean</div>
-              <div id='email'>@bob.lens</div>
-              <div className='basic_info_data_up'>
-                <div>Music is my life</div>
+            <div className='category_container'>
+              <div>
+                <TextField
+                  id="standard-read-only-input"
+                  defaultValue="AI Tags"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  variant="standard"
+                />
               </div>
               <div className='category_data_list'>
                 <AITagsCard
@@ -87,43 +90,48 @@ export default function ProfileAppPage() {
                   fontSize={30}
                 />
               </div>
-              <div className='basic_info_data_down'>
-                <div id='address'>0x6C956B990179fcdfab0C323615f2a93af4A9a0ad |</div>
-                <div id='followings'>10 Followings |</div>
-                <div id='followers'>9,765 Followers |</div>
-                <div id='birthday'>Joined March 2020</div>
+            </div>
+            <div className='category_container'>
+              <div>
+                <TextField
+                  id="standard-read-only-input"
+                  defaultValue="Activities"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  variant="standard"
+                />
+              </div>
+              <div className='category_data_list'>
+                <div className='category_data_list_cl'>
+                  <ActivityCard
+                    name={`Posts`}
+                    number={76}
+                    variation={11}
+                  />
+                  <ActivityCard
+                    name={`Mirrors`}
+                    number={19231}
+                    variation={-56.6}
+                  />
+                  <ActivityCard
+                    name={`Comments`}
+                    number={156}
+                    variation={17}
+                  />
+                </div>
               </div>
             </div>
-            <div className='profile_list'>
-              <div className='category_container'>
-                <div>
-                  <TextField
-                    id="standard-read-only-input"
-                    defaultValue="Achievements"
-                    InputProps={{
-                      readOnly: true,
-                    }}
-                    variant="standard"
-                  />
-                </div>
-                <div className='category_data_list'>
-
-                </div>
-              </div>
-              <div className='category_container'>
-                <div>
-                  <TextField
-                    id="standard-read-only-input"
-                    defaultValue="AI Tags"
-                    InputProps={{
-                      readOnly: true,
-                    }}
-                    variant="standard"
-                  />
-                </div>
-                <div className='category_data_list'>
-
-                </div>
+            <div className='category_container'>
+              <div>
+                <TextField
+                  id="standard-read-only-input"
+                  defaultValue="Benefits"
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  variant="standard"
+                />
               </div>
               <div className='category_data_list cl'>
                 <div className='category_data_list_cl'>
